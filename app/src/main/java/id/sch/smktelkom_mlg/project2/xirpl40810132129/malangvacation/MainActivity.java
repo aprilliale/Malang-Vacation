8d9ad54314bf0e210faba068b31f320b07c9b012
eunhyukee44 <aprillia.le@gmail.com>
2017-03-04 18:51:03 +0700

    Initial commit

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/MainActivity.java
@@ -3,11 +3,23 @@ package id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+    String id;
+    String[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String hasil = null;
+        InputStream is = null;
+        StringBuilder sb = null;
+
+        try {
+
+        }
     }
 }
